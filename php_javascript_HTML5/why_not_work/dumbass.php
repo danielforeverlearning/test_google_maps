3335c1f36b83faa7a98bf069bb5f8c8e4222b2f5
danielforeverlearning <danielforeverlearning@gmail.com>
2019-09-26 08:30:28 -1000

    Add files via upload

---
+++ php_javascript_HTML5/why_not_work/dumbass.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="dumbasssubmit.php" method="post">
+Latitude: <input type="text" name="latitude"><br>
+Longitude: <input type="text" name="longitude"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ php_javascript_HTML5/why_not_work/dumbasssubmit.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>dumbass</title>
+  </head>
+  <body>
+		<p>hi how are you</p>
+		<p><?php echo $_POST["latitude"]; ?></p>
+		<p><?php echo $_POST["longitude"]; ?></p>
+  </body>
+</html>
